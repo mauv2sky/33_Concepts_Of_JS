89c59f89e2a95c9f24cb39d16c00cf6b24799849
mauv2sky <mauv2sky@gmail.com>
2021-04-22 22:40:09 +0900

    call stack; add image folder, sorce codes

---
+++ 01_CallStack/callStack.js
@@ -0,0 +1,16 @@
+function foo() {
+    console.log("function foo is called");
+    throw new Error("oops!");
+}
+
+function bar() {
+    console.log("function bar is called");
+    foo();
+}
+
+function baz() {
+    console.log("function baz is called");
+    bar();
+}
+
+baz();
---
+++ 01_CallStack/overflow.js
@@ -0,0 +1,5 @@
+function overflow(){
+    overflow();
+}
+
+overflow();
